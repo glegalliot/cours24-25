2105a7eb3bf3014e26a4485f2660af299cc20103
glegalliot <gaelle.le-galliot@ac-rennes.fr>
2025-05-03 21:34:11 +0200

    init du repo

---
+++ Premiere Spe/02-Second_degre/scripts/nb_racines.py
@@ -0,0 +1,17 @@
+a = float(input("entrez a"))
+b = float(input("entrez b"))
+c = float(input("entrez c"))
+delta = b ** 2 - 4 * a * c
+if delta > 0:
+    print(2)
+elif delta == 0:
+    print(1)
+else:
+    print(0)
+
+
+truc = 2
+
+print("Ceci est la valeur de truc : ",truc, "et voilà")
+
+print(f"Ceci est la valeur de truc : {truc} et voilà")
---
+++ Term Comp/Modèles groupes de prod/Dossier cigognes/Cigognes_Version_tex/Cigognes_Version_tex/T_EnsSC_Thème_3_4_Cigogne.py
@@ -0,0 +1,11 @@
+
+
+def extinction():
+    n=0
+    u=145
+    while u>=5 :
+        n=n+1
+        u=u-12.2
+    return (1960+n)
+        
+
